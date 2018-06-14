025da136505c718952268b4229bb62d798a9b668
Lorena <lorena.giddens@outlook.com>
2018-06-14 13:58:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matched_array = []
+    array.each do |word|
+      if word.split("").sort == self.word.split("").sort
+        matched_array << word
+      end
+    end
+    matched_array
+  end
+end
