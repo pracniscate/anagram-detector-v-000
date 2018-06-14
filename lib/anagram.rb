51baef467913efa46f1df431a426c58099e46a4e
Lorena <lorena.giddens@outlook.com>
2018-06-14 13:51:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @result = []
+  end
+
+  def match(array)
+    array.each do |word|
+      @result << word if word.split("").sort == @word.split("").sort
+    end
+  end
+  @result
+end
